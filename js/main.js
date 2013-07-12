dd84b2e0d91dae8840907649d8170d0d2dc6e6c4
Gerald Duane Haagsma II <ghaagsma@umich.edu>
2013-07-12 16:17:02 -0400

    Working header tabs

--- js/main.js
+++ js/main.js
@@ -1,12 +1,4 @@
 
-function SignIn(emailInputId, passwordInputId)
-{
-	email = document.getElementById(emailInputId).value;
-	password = document.getElementById(passwordInputId).value;
-	alert(email);
-}
-
-function ClearSignInModal()
-{
-	$("#signUpForm input[type=text], #signUpForm input[type=password]").val("");
+function ClearSignInModal() {
+    $("#signUpForm input[type=text], #signUpForm input[type=password]").val("");
 }
