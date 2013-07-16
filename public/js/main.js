f53bd507cb4a198f6436c4d4a9a4a7e130196baf
Gerald Duane Haagsma II <ghaagsma@umich.edu>
2013-07-15 23:31:57 -0400

    probably doesn't work, but some code is there

--- public/js/main.js
+++ public/js/main.js
@@ -1,23 +1,114 @@
-function CreateWorkflow() {
-    $('#workflowModal').modal('hide');
-}
-
 function StitchController($scope) {
+    $scope.workflows = [];
     $scope.stitches = [];
 
     $scope.NewWorkflow = function() {
         $scope.stitches = [];
+        $('#alertDiv').remove();
+        $('#workflowNameInput').val("");
     };
 
     $scope.AddStitch = function() {
         $scope.stitches.push({
             name: 'New Stitch'
         });
+    };
 
-        $('.rss-element-' + $scope.stitches.length() - 1).show();
-        $('.xml-element-' + $scope.stitches.length() - 1).hide();
-        $('.sms-element-' + $scope.stitches.length() - 1).hide();
+    $scope.DeleteStitch = function(index) {
+        $scope.stitches.splice(index, 1);
+
+        // Not working...
+        $('.accordion-body').each(function() {
+            $(this).removeClass("in");
+        });
     }
+
+    $scope.CreateWorkflow = function() {
+        var workflowName = $('#workflowNameInput').val();
+        if (!workflowName) {
+            var message = "Please give your workflow a name.";
+            $('#alertPlaceholder').html('<div id="alertDiv" class="alert alert-danger"><a class="close" data-dismiss="alert">×</a><span>' + message + '</span></div>')
+            return;
+        }
+
+        var newStitches = [];
+
+        for (var i = 0; i < $scope.stitches.length; i++) {
+            var newStitch = new Object();
+            newStitch.name = $("#stitchNameInput" + i).val();
+
+            // Get RSS
+            if ($("#stitchTypeSelector" + i).val() == 0) {
+                newStitch.stitchTemplateId = '{5F6486EB-FD30-4024-8D07-66B398784C4D}';
+                newStitch.sequence = i;
+                newStitch.inputs = [{
+                    name: 'Feed URL',
+                    value: $("#rssUrlInput" + i).val()
+                }];
+            }
+            // Parse XML
+            else if ($("#stitchTypeSelector" + i).val() == 1) {
+                newStitch.stitchTemplateId = '{240A7FBB-77FB-42AF-84C1-36D423C53778}';
+                newStitch.sequence = i;
+                newStitch.inputs = [{
+                    name: 'Document URL',
+                    value: $("#xmlUrlInput" + i).val()
+                }, {
+                    name: 'Xpath',
+                    value: $("#xmlPathInput" + i).val()
+                }];
+            }
+            // Send SMS
+            else if ($("#stitchTypeSelector" + i).val() == 2) {
+                newStitch.stitchTemplateId = '{6CAC6474-45C4-4F4B-A965-E780AE82B2DC}';
+                newStitch.sequence = i;
+                newStitch.inputs = [{
+                    name: 'Phone Number',
+                    value: $("#smsInput" + i).val()
+                }, {
+                    name: 'Message',
+                    value: 'Notification'
+                }];
+            }
+            // ???
+            else {
+                window.alert("This shouldn't happen");
+            }
+
+            newStitches.push(newStitch);
+        }
+
+        $scope.workflows.push({
+            lastRunDate: null,
+            lastRunStep: 0,
+            lastRunComplete: false,
+            name: $('#workflowNameInput').val(),
+            stitches: newStitches
+        });
+
+        var wFlow = $scope.workflows.pop();
+
+        // Send new workflow to backend
+        $.ajax({
+            url: "/api/workflows/",
+            type: "post",
+            data: wFlow,
+            success: function() {
+                alert("success");
+                $("#result").html('submitted successfully');
+            },
+            error: function() {
+                alert("failure");
+                $("#result").html('there is error while submit');
+            }
+        });
+
+        $scope.workflows.push(wFlow);
+
+        $('#workflowModal').modal('hide');
+        $('#alertDiv').remove();
+        $('#workflowNameInput').val("");
+    };
 }
 
 $(function() {
